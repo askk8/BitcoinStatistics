abe520ee78f99c5489cac69e83f77f56c27f29cc
Adam Kiniouar <adam.kiniouar@alimakgroup.com>
2020-09-15 17:03:34 +0200

    Adding HttpGet

---
+++ BTCStats/Data/Models/PriceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BTCStats.Data.Models
+{
+    public class PriceModel
+    { 
+        public DateTime bpi { get; set; }
+
+        public PriceModel()
+        {
+        }
+    }
+}
--- BTCStats/Startup.cs
+++ BTCStats/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BTCStats.Data;
+using System.Net.Http;
 
 namespace BTCStats
 {
@@ -29,6 +30,7 @@ namespace BTCStats
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<HttpClient>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
